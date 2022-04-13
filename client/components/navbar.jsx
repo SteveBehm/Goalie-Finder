155ab34a0ad926f7fe38f340abe4c98a86ee09c1
Steve Behm <stevtbehm1@gmail.com>
2022-04-13 00:30:01 +0000

    completed issue-2-list-of-users and commenting.

--- client/components/navbar.jsx
+++ client/components/navbar.jsx
@@ -3,6 +3,11 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 
+// This is the class that will generate the method to create the Nabar
+// the navbar is a bootstrap react component
+// it has a logo and two links when seen on a desktop
+// it has a logo and hamburger menu when seen in mobile few
+// the hamburger menu drops down to see the two links
 export default class AppNavbar extends React.Component {
   render() {
     return (
@@ -14,8 +19,8 @@ export default class AppNavbar extends React.Component {
             <Nav className="me-auto">
             </Nav>
             <Nav>
-              <Nav.Link href="#edit-profile">Edit Profile</Nav.Link>
-              <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+              <Nav.Link className='edit-profile' href="#edit-profile">Edit Profile</Nav.Link>
+              <Nav.Link className='sign-out' href="#sign-out">Sign Out</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
